3d7a5ae399e5cff27d3f93b54f54de524a64c6e6
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-28 17:19:50 +0200

    1 GROUP BY

--- queries.sql
+++ queries.sql
@@ -24,7 +24,7 @@ SELECT COUNT(*) FROM `teachers` WHERE `phone` IS NULL;
 --*   EX - Query con GROUP BY
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT COUNT(*) AS `iscritti`, YEAR(`enrolment_date`) AS `anno` FROM `students` GROUP BY YEAR(`enrolment_date`);
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame

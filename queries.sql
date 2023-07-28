ff8669e8d66f16180936b155b56eaf353c93def0
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-28 17:43:30 +0200

    3 GROUP BY

--- queries.sql
+++ queries.sql
@@ -28,5 +28,5 @@ SELECT COUNT(*) AS `iscritti`, YEAR(`enrolment_date`) AS `anno` FROM `students` 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 SELECT COUNT(*) AS `insegnanti`,`office_address` AS `indirizzo` FROM `teachers` GROUP BY `office_address`;
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT `exam_id` AS `appello`, AVG(`vote`) AS `media_voti` FROM `exam_student` GROUP BY `exam_id`;
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento

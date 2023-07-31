7e2b440a439e6d20d9701275d7aef95076a3a826
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-31 17:05:18 +0200

    4 JOIN

--- queries.sql
+++ queries.sql
@@ -58,7 +58,13 @@ ON `course_teacher`.`course_id` = `courses`.`id`
 WHERE `teachers`.`name` = 'Fulvio'
 AND `teachers`.`surname` = 'Amato';
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-
+SELECT `students`.`surname` AS `cognome`, `students`.`name` AS `nome`, `degrees`.`name` AS `laurea`, `departments`.`name` AS `dipartimento`
+FROM `students`
+JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`, `students`.`name`;
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)

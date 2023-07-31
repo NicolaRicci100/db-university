2b1d4b946f4b0317fa203029e28da73d1cb93554
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-31 15:05:32 +0200

    aggiunto diagramma e consegne JOIN query

--- queries.sql
+++ queries.sql
@@ -31,3 +31,21 @@ SELECT COUNT(*) AS `insegnanti`,`office_address` AS `indirizzo` FROM `teachers` 
 SELECT `exam_id` AS `appello`, AVG(`vote`) AS `media_voti` FROM `exam_student` GROUP BY `exam_id`;
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 SELECT COUNT(`name`) AS `corsi`, `department_id` AS `dipartimento` FROM `degrees` GROUP BY `department_id`;
+
+
+
+--*   EX - Query con JOIN
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
